d8e0ca3cc455d52aa10bd298011bece2865a6c45
acolistro <arcolistro@gmail.com>
2020-04-01 16:13:04 -0700

    two new migrations
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>
    Co-authored-by: Alex Skreen <skreenalex@gmail.com>
    Co-authored-by: Mariam <maralaeddin@gmail.com>

---
+++ MessageBoardAPI/Migrations/20200401222316_Stuff.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageBoardAPI.Migrations
+{
+    public partial class Stuff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Message_Groups_GroupId",
+                table: "Message");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Message",
+                table: "Message");
+
+            migrationBuilder.DropColumn(
+                name: "MessageId",
+                table: "Groups");
+
+            migrationBuilder.RenameTable(
+                name: "Message",
+                newName: "Messages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Message_GroupId",
+                table: "Messages",
+                newName: "IX_Messages_GroupId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Messages",
+                table: "Messages",
+                column: "MessageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Groups_GroupId",
+                table: "Messages",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Groups_GroupId",
+                table: "Messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Messages",
+                table: "Messages");
+
+            migrationBuilder.RenameTable(
+                name: "Messages",
+                newName: "Message");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_GroupId",
+                table: "Message",
+                newName: "IX_Message_GroupId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MessageId",
+                table: "Groups",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Message",
+                table: "Message",
+                column: "MessageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Message_Groups_GroupId",
+                table: "Message",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ MessageBoardAPI/Migrations/20200401230846_NewDataKey.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageBoardAPI.Migrations
+{
+    public partial class NewDataKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Groups_GroupId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_GroupId",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GroupId",
+                table: "Messages",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<int>(
+                name: "GroupId1",
+                table: "Messages",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_GroupId1",
+                table: "Messages",
+                column: "GroupId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Groups_GroupId1",
+                table: "Messages",
+                column: "GroupId1",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Groups_GroupId1",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_GroupId1",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "GroupId1",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GroupId",
+                table: "Messages",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_GroupId",
+                table: "Messages",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Groups_GroupId",
+                table: "Messages",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
