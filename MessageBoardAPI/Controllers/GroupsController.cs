beb20381a1b8b040999f0eeb158f532760d8393e
acolistro <arcolistro@gmail.com>
2020-04-01 16:14:04 -0700

    two new migrations
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>
    Co-authored-by: Alex Skreen <skreenalex@gmail.com>
    Co-authored-by: Mariam <maralaeddin@gmail.com>

--- MessageBoardAPI/Controllers/GroupsController.cs
+++ MessageBoardAPI/Controllers/GroupsController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using MessageBoardAPI.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace MessageBoardAPI.Controllers
 {
+  [Route("api/[controller]")]
+  [ApiController]
   public class GroupsController : ControllerBase
   {
     private MessageBoardAPIContext _db;
@@ -14,44 +15,22 @@ namespace MessageBoardAPI.Controllers
     {
       _db = db;
     }
-    [HttpGet]
-    public ActionResult<IEnumerable<Group>> Get(string topic)
+    [HttpGet("{id}")]
+    public ActionResult<Group> Get(int id)
     {
-      var query = _db.Groups.AsQueryable();
-
-      if (topic != null)
-      {
-        query = query.Where(entry => entry.Topic == topic);
-      }
-      
-      return query.ToList();
+      return _db.Groups.FirstOrDefault(entry => entry.GroupId == id);
     }
-
     [HttpPost]
     public void Post([FromBody] Group group)
     {
       _db.Groups.Add(group);
       _db.SaveChanges();
     }
+
     [HttpGet("{id}")]
     public ActionResult<Group> Get(int id)
     {
       return _db.Groups.FirstOrDefault(entry => entry.GroupId == id);
-    }
-
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] Group group)
-    {
-      group.GroupId = id;
-      _db.Entry(group).State = EntityState.Modified;
-      _db.SaveChanges();
-    }
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-      var groupDelete = _db.Groups.FirstOrDefault(entry => entry.GroupId == id);
-      _db.Groups.Remove(groupDelete);
-      _db.SaveChanges();
     }
   }
 }
--- MessageBoardAPI/Controllers/ValuesController.cs
+++ MessageBoardAPI/Controllers/ValuesController.cs
@@ -28,7 +28,6 @@ namespace MessageBoardAPI.Controllers
         [HttpPost]
         public void Post([FromBody] string value)
         {
-        Console.WriteLine(value);
         }
 
         // PUT api/values/5
